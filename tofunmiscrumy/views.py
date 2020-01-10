c8cce676a96fee096964eef1991962ad01baf267
toffy <oketofoke@gmail.com>
2020-01-10 19:49:19 +0100

    first commit

---
+++ tofunmiscrumy/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+def get_grading_parameters(request):
+    return HttpResponse("Welcome to Django")
+
+def index(request):
+    return HttpResponse("Welcome to Django") 
+
