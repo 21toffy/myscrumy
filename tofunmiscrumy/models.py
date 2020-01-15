e5e51f85ef9b694ce312aacdff9387383e422221
toffy <oketofoke@gmail.com>
2020-01-15 15:55:15 +0100

    migrated table and hopefully i have completed the labs for this week

--- tofunmiscrumy/models.py
+++ tofunmiscrumy/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
-# Create your models here.
+class GoalStatus(models.Model):
+    status_name = models.CharField(max_length=50)
+
+class ScrumyGoal(models.Model):
+    goal_name = models.CharField(max_length=50) 
+    goal_id = models.IntegerField ()
+    created_by = models.CharField(max_length=50)
+    moved_by = models.CharField(max_length=50)
+    owner = models.CharField(max_length=50)
+    goal_status = models.ForeignKey(GoalStatus, on_delete = models.PROTECT)
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name="user_goal")
+    
+class ScrumyHistory(models.Model):
+    created_by = models.CharField(max_length=50)
+    moved_by = models.CharField(max_length=50)
+    owner = models.CharField(max_length=50)
+    goal = models.ForeignKey(ScrumyGoal, on_delete=models.CASCADE)
+
+
