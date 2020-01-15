0a906b92d8239926d823526b7cee88c4d4d4e3f8
toffy <oketofoke@gmail.com>
2020-01-15 02:18:12 +0100

    mysql database

--- tofunmiscrumy/views.py
+++ tofunmiscrumy/views.py
@@ -1,7 +1,4 @@
 from django.http import HttpResponse
+
 def get_grading_parameters(request):
     return HttpResponse("Welcome to Django")
-
-def index(request):
-    return HttpResponse("Welcome to Django") 
-
