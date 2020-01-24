b5a2bc00b108e69ba3867c4638967250c190f0e7
toffy <oketofoke@gmail.com>
2020-01-24 17:08:53 +0100

    lab 10 still not giving me 100percent/but this is lab11 submission

--- tofunmiscrumy/views.py
+++ tofunmiscrumy/views.py
@@ -1,11 +1,11 @@
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from .models import GoalStatus, ScrumyGoals, ScrumyHistory
-# def index(request):
-#     goal_name = ScrumyGoals.objects.filter(goal_name='Learn Django')
-#     return HttpResponse(goal_name)
-
 def index(request):
+    goal_name = ScrumyGoals.objects.filter(goal_name='Learn Django')
+    return HttpResponse(goal_name)
+
+def scrumygoals(request):
     scrumygoals =  ScrumyGoals.objects.all()
     return HttpResponse(scrumygoals)
 
