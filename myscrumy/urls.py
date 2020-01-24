da2969babe1bf1660d1d532020a762a0fb7d99d3
toffy <oketofoke@gmail.com>
2020-01-24 23:20:33 +0100

    kikoly more correctionscorrectio and movegoal/id corrrection

--- myscrumy/urls.py
+++ myscrumy/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tofunmiscrumy.urls')),
+    path('tofunmiscrumy/', include('tofunmiscrumy.urls')),
 ]
--- tofunmiscrumy/views.py
+++ tofunmiscrumy/views.py
@@ -2,18 +2,19 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from .models import GoalStatus, ScrumyGoals, ScrumyHistory
 
-def query_filter(request):
-    name = ScrumyGoals.objects.filter(goal_name='Learn Django')
-    return HttpResponse(name)
+def query_filter(request, *args, **kwargs):
+    name = ScrumyGoals.objects.filter(goal_name='Learn Django').get()
+    return HttpResponse(f"{name}")
+
+
+
 
 
 def index(request):
     goal_name = ScrumyGoals.objects.filter(goal_name='Learn Django')
     return HttpResponse(goal_name)
 
-def scrumygoals(request):
-    scrumygoals =  ScrumyGoals.objects.all()
-    return HttpResponse(f'{scrumygoals}')
+
 
 
 
@@ -21,3 +22,58 @@ def scrumygoals(request):
 def move_goal(request, goal_id):
     goal_id = ScrumyGoals.objects.get(goal_id=goal_id)
     return HttpResponse(goal_id)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def scrumygoals(request):
+    scrumygoals =  ScrumyGoals.objects.all()
+    return HttpResponse(f'{scrumygoals}')
